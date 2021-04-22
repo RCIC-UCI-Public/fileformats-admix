0d0b6723f0b90caeabffaa6e302c49cd3694d4fd
Philip Papadopoulos <ppapadop@uci.edu>
2021-04-22 11:53:51 -0700

    Automatically build nco for gcc 8

--- yamlspecs/SubmakeDefs.mk
+++ yamlspecs/SubmakeDefs.mk
@@ -1,5 +1,5 @@
-# These are common defs used in the top-level Makefile and 
-# and Makefile.intel 
-PKGS_PER_COMPILER = hdf5 hdf5-parallel hdf5-module hdf5-parallel-module
+# These are common defs used in the compiler-specific Makefiles and 
+#  
+PKGS_PER_COMPILER += hdf5 hdf5-parallel hdf5-module hdf5-parallel-module
 PKGS_PER_COMPILER += netcdf-c netcdf-c-module netcdf-fortran ioapi ioapi-module udunits udunits-module
 TARGETS_PER_COMPILER = $(addsuffix .pkg, $(PKGS_PER_COMPILER))
